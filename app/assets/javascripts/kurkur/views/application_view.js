eb77536d4d5ec06557df600ff150fb2051ff005e
Andri Kurnia <andrikurnia@live.com>
2014-08-18 13:22:24 +0700

    delete log

--- app/assets/javascripts/kurkur/views/application_view.js
+++ app/assets/javascripts/kurkur/views/application_view.js
@@ -1,6 +1,5 @@
 Kurkur.ApplicationView = Ember.View.extend({
   not404: function() {
-    console.log(Kurkur.currentPath)
     return Kurkur.currentPath != "fourOhFour"
   }.property('currentPath')
 });
