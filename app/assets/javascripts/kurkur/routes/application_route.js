8fe10137dfbd1f9e70fd2d2cb6d9293ae2e2d389
Andri Kurnia <andrikurnia@live.com>
2014-08-20 10:06:49 +0700

    seo

--- app/assets/javascripts/kurkur/routes/application_route.js
+++ app/assets/javascripts/kurkur/routes/application_route.js
@@ -1,2 +1,4 @@
-Kurkur.ApplicationRoute = Ember.Route.extend({
+Kurkur.ApplicationRoute = Ember.Route.extend(Kurkur.SeoMixin, {
+  title: 'Andri Kurnia',
+  description: 'Andri Kurnia - Personal website and some good article or blog for Web developer, Enginner, and of course my favorite game Dota 2'
 });
