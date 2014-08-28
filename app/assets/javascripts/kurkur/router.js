96ffe4855ab4203fe8b569fb8adac4f2402eb55b
Andri Kurnia <andrikurnia@live.com>
2014-08-28 14:59:24 +0700

    update

--- app/assets/javascripts/kurkur/router.js
+++ app/assets/javascripts/kurkur/router.js
@@ -2,7 +2,7 @@
 
 Kurkur.Router.map(function() {
   // Root
-  // this.route('index', {path: 'andrkrn'})
+  this.route('index', {path: '/'})
   // Sessions
   this.route('sign_in');
   this.resource('sessions', function() {
