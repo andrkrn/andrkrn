d79532bf906adb31fe638ee367f4ccbcde75be6d
Andri Kurnia <andrikurnia@live.com>
2014-08-29 11:05:20 +0700

    permit all params?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  ActionController::Parameters.permit_all_parameters = true
+  
   before_filter :redirect_from_heroku
+  before_filter :check_escaped_fragment
 
   def redirect_from_heroku
     redirect_to "http://www.andrikurnia.com/", status: 301 if request.domain == "herokuapp.com"
+  end
+
+  def check_escaped_fragment
+    puts "[DATA] #{params["_escaped_fragment_"]}"
   end
 end
