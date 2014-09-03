787575be0b37311c7fe377950819a8fc306902e1
Andri Kurnia <andrikurnia@live.com>
2014-09-03 11:51:52 +0700

    update

--- app/assets/javascripts/kurkur/routes/article_route.js
+++ app/assets/javascripts/kurkur/routes/article_route.js
@@ -1,5 +1,9 @@
 Kurkur.ArticleRoute = Ember.Route.extend({
   model: function(params) {
     return this.store.find('article', params.article_id);
+  },
+
+  afterModel: function(model) {
+    $(document).attr('title', model._data.title + ' | Blog');
   }
 });
