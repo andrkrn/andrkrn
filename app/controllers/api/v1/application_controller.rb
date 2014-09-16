195cfeb2c7133a983846c1f11778d86500c0826a
Andri Kurnia <andrikurnia@live.com>
2014-09-16 13:32:09 +0700

    fix direct access api

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -10,10 +10,7 @@ class Api::V1::ApplicationController < ActionController::API
 
   def allow_request_from_domain
     if Rails.env == "production"
-      puts "[IP] #{request.ip}"
-      puts "[IP] #{request.remote_ip}"
-      puts "[IP] #{request.referrer}"
-      unless ['104.28.4.69', '104.28.5.69'].include?(request.ip)
+      if URI(request.referrer).host == "www.andrikurnia.com"
         ensure_authenticated_user
       end
     elsif Rails.env == "development"
